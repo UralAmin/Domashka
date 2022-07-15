4218064cab2c15e928ce74073ee296a1ae740109
Aminefff <aminefff@Yandex.ru>
2022-07-16 01:05:59 +0500

    1 часть 2 домашки (проверил числа - работает)

---
+++ src/Domashka_prodv.java
@@ -0,0 +1,15 @@
+public class Domashka_prodv {
+    public static void main(String[] args) {
+
+        String text = "234";
+        int a = Integer.valueOf(text);
+        System.out.println(a);
+
+
+
+
+
+    }
+
+
+}
