813df1e9de35c22fedff7657e1a2c6878d0d987b
Aminefff <aminefff@Yandex.ru>
2022-07-16 00:32:10 +0500

    1 часть домашки

---
+++ src/Domashka_baza.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+
+public class Domashka_baza {
+    public static void main(String[] args) {
+        String hi = "                Hello ";
+        String world = " WoRld!";
+        char newLine = '\n';
+        world = world.toLowerCase();
+        String hiWorld = hi + world;
+        hiWorld = hiWorld.trim();
+        System.out.println(hiWorld+'\n'+hiWorld+'\n'+hiWorld);
+
+    }
+
+
+
+}
