7e8a0c043e56a900e14373c2a52105db2ee91bd0
Aminefff <aminefff@Yandex.ru>
2022-07-16 13:35:29 +0500

    4 часть 2ой домашки.

--- src/Domashka_prodv.java
+++ src/Domashka_prodv.java
@@ -20,6 +20,20 @@ public class Domashka_prodv {
         int[] array2 = new int[]{2,3,2,17,15};
 
 
+        //четверая задача
+
+        String str ="Hello world";
+        String str1 = str.replace('l', 'r');
+        System.out.println(str1);
+
+
+        //Верхний регистр
+        str = str.toUpperCase();
+        System.out.println(str);
+
+        //обрезка до 8 символов
+        str = str.substring(0,8);
+        System.out.println(str);
 
 
     }
